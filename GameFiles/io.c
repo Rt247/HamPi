fd29195bbdaa7686d15198b47cb74eb0e331bcb8
Subhash <nsn116@ic.ac.uk>
2017-07-07 16:12:48 +0100

    Commented out functions in io.c

--- GameFiles/io.c
+++ GameFiles/io.c
@@ -5,6 +5,13 @@
 #define MAX_ROW_COL_DIGITS 2
 #define BASE10 10
 
+/**
+ * Returns a heap allocated string representation of the map
+ * @param map the map whose string representation is to be returned
+ * @param rows the number of rows in the map
+ * @param cols the number of columns in the map
+ * @return pointer to the heap allocated string representation of the map
+ */
 static char **toString(squareState **map, int rows, int cols) {
   char **mapAsString = (char **) malloc((size_t)((rows + 2) * sizeof(char *)));
   mapAsString[0] = (char *) calloc(MAX_ROW_COL_DIGITS + 1, sizeof(char));
@@ -26,6 +33,13 @@ static char **toString(squareState **map, int rows, int cols) {
   return mapAsString;
 }
 
+/**
+ * Writes the given map into the file whose filename is passed as a parameter
+ * @param fileName the name of the file to be written into
+ * @param map the map to be written into the file
+ * @param rows the number of rows in the map
+ * @param cols the number of columns in the map
+ */
 void writeMap(char fileName[], squareState **map, int rows, int cols) {
   char **mapAsString = toString(map, rows, cols);
   FILE *mapFilePtr = fopen(fileName, "w");
@@ -46,6 +60,13 @@ void writeMap(char fileName[], squareState **map, int rows, int cols) {
   free(mapAsString);
 }
 
+/**
+ * Reads a map from the file whose filename is passed as a parameter
+ * @param fileName the name of the file to be read from
+ * @param rows the number of rows in the map
+ * @param cols the number of columns in the map
+ * @return a pointer to a heap allocated map of squareStates
+ */
 squareState **readMap(char fileName[], int *rows, int *cols) {
   FILE *mapFilePtr = fopen(fileName, "r");
   if (mapFilePtr == NULL) {
@@ -77,6 +98,11 @@ squareState **readMap(char fileName[], int *rows, int *cols) {
   return map;
 }
 
+/**
+ * Returns the size of the binary file passed as input
+ * @param binFile file pointer to the binary file whose size is to be computed
+ * @return the size of the file
+ */
 static int getFileSize(FILE *binFile) {
   fseek(binFile, 0L, SEEK_END);
   int size = (int) ftell(binFile);
@@ -84,7 +110,13 @@ static int getFileSize(FILE *binFile) {
   return size;
 }
 
-//Writes the solution of the level to be saved to a binary file
+/**
+ * Writes the solution of the level to be saved, to a binary file
+ * @param fileName the name of the file to be written into
+ * @param solArray the solution of the given map represented as player
+ * coordinates
+ * @param size the number of entries in the solution array
+ */
 void writeSolution(char fileName[], playerPosition *solArray[], int size) {
   FILE *mapFilePtr = fopen(fileName, "wb");
   if (mapFilePtr == NULL) {
@@ -101,7 +133,12 @@ void writeSolution(char fileName[], playerPosition *solArray[], int size) {
   }
 }
 
-//Reads the solution to a saved level from a binary file
+/**
+ * Reads the solution to a saved level from a binary file
+ * @param fileName the name of the file to be read from
+ * @return a pointer to a heap allocated memory containing the solution of the
+ * map that has been saved
+ */
 playerPosition **readSolution(char fileName[]) {
   FILE *mapFilePtr = fopen(fileName, "rb");
   if (mapFilePtr == NULL) {
@@ -123,7 +160,14 @@ playerPosition **readSolution(char fileName[]) {
   }
   return solArray;
 }
-//TODO
+
+/**
+ * Writes the linked list representing the leaderboard into the file identified
+ * by the file name passed as a parameter
+ * @param fileName the name of the file in which the leaderboard is
+ * to be written into
+ * @param head the head of the linked list representing the leaderboard
+ */
 void writeLeaderBoard(char *fileName, leaderBoard *head) {
   FILE *leaderboardFilePtr = fopen(fileName, "wb");
   if (leaderboardFilePtr == NULL) {
@@ -142,9 +186,15 @@ void writeLeaderBoard(char *fileName, leaderBoard *head) {
   }
 }
 
-//TODO
 // PRE: leaderboard with three elements has been allocated, with head being the
 // pointer to the first element.
+/**
+ * Reads the leaderboard from a file which is identified by the file name
+ * passed as a parameter
+ * @param fileName the name of the file in which the leaderboard is
+ * to be read from
+ * @param head the head of the linked list representing the leaderboard
+ */
 void readLeaderBoard(char *fileName, leaderBoard *head) {
   FILE *leaderboardFilePtr = fopen(fileName, "rb");
   if (leaderboardFilePtr == NULL) {
